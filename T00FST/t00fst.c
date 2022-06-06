0082d86d2166db88ca000237131953e28b7043dc
Vitaly A. Galinsky <cgsg@yandex.ru>
2022-06-06 10:01:59 +0300

    Add drawing samples.

--- T00FST/t00fst.c
+++ T00FST/t00fst.c
@@ -2,10 +2,21 @@
 #include <stdio.h>
 #include <windows.h>
 
-void main( void )
+VOID main( VOID )
 {
-  int x;
-  char Buf[102];
+  INT x, y;
+  CHAR Buf[102];
+  RECT rc;
+  HANDLE h;
+  HDC hDC;
+
+  hDC = GetDC(NULL);
+
+  for (y = 0; y < 10; y++)
+    for (x = 0; x < 1920; x++)
+      SetPixelV(hDC, x + 1920 * 2, y, RGB(x, y, 255 - (x + y) / 2));
+
+  ReleaseDC(NULL, hDC);
 
   printf("Input X:");
   scanf("%d", &x);
